b880339d4a88771407211ea3220016d8a248568a
Rob K <105181695+coderrob22@users.noreply.github.com>
2022-08-25 09:23:45 -0400

    Add files via upload

---
+++ Background Picker/js/main.js
@@ -0,0 +1,103 @@
+document.getElementById('HeyArnold').onclick = heyArnold
+document.getElementById('Rugrats').onclick = rugRats
+document.getElementById('PowerRangers').onclick = powerRangers
+document.getElementById('Recess').onclick = Recess
+document.getElementById('DexterLab').onclick = dexterLab
+document.getElementById('Arthur').onclick = Arthur
+document.getElementById('Freshprince').onclick = freshPrince
+document.getElementById('Savedbybell').onclick = savedByBell
+document.getElementById('PinkyBrain').onclick = pinkyBrain
+
+
+
+function heyArnold() {
+  document.querySelector('body').style.backgroundImage ="url('Hey Arnold.jpeg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize ="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+  
+}
+
+function rugRats() {
+  document.querySelector('body').style.backgroundImage="url('Rugrats.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize = "100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+}
+
+function powerRangers() {
+  document.querySelector('body').style.backgroundImage ="url('power rangers.jpg')"
+  document.querySelector('body').style.color = 'gray'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+function Recess() {
+  document.querySelector('body').style.backgroundImage ="url('recess.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+function dexterLab() {
+  document.querySelector('body').style.backgroundImage="url('Dexlab.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+function Arthur() {
+  document.querySelector('body').style.backgroundImage="url('arthur.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+function freshPrince() {
+  document.querySelector('body').style.backgroundImage="url('Fresh Prince.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+function savedByBell() {
+  document.querySelector('body').style.backgroundImage="url('Saved by the bell.jpg')"
+  document.querySelector('body').style.color = 'gray'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+}
+function pinkyBrain() {
+  document.querySelector('body').style.backgroundImage="url('pinky and the brain.jpg')"
+  document.querySelector('body').style.color = 'black'
+  document.querySelector('body').style.backgroundRepeat= "no-repeat"
+  document.querySelector('body').style.backgroundPosition= "center"
+  document.querySelector('body').style.backgroundSize="100% 100%"
+  document.getElementById('tv').style.visibility="hidden"
+
+
+}
+
+console.log("Hello")
